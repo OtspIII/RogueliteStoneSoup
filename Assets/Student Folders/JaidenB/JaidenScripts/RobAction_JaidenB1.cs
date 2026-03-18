e2b840e52e22fee9a7541d439c76d752fe8a1d6e
HurriHydra <JbryantOfficial2004@gmail.com>
2026-03-18 12:17:09 -0400

    Rob Action update

--- Assets/Student Folders/JaidenB/JaidenScripts/RobAction_JaidenB1.cs
+++ Assets/Student Folders/JaidenB/JaidenScripts/RobAction_JaidenB1.cs
@@ -1,31 +1,23 @@
 using UnityEngine;
 
-public class RobAction_JaidenB : ActionScript
+public class RobAction_JaidenB : SwingAction
 {
     public RobAction_JaidenB(ThingInfo who, EventInfo e = null) 
     {
-       Setup(Actions.RobAction_JaidenB, who, true);
+       Setup(Actions.RobAction_JaidenB, who);
        Anim = "Swing";
+        //Who.TakeEvent(God.E(EventTypes.AddScore).Set(-1).Set(e.GetThing()), true);
+        
     }
 
-    public override void OnRun()
-    {
-        base.OnRun();
-        if (Phase < 1)
-        {
-            Who.Thing.MoveForwards();
-        }
-    }
+   
 
-    public override void ChangePhase(int newPhase)
+    public override void HitBegin(GameCollision col)
     {
-        base.ChangePhase(newPhase);
-        if (Phase == 0)
-        {
-            MoveMult = 1;
-        }
-        else
-            MoveMult = 0;
+        base.HitBegin(col);
+        //if (col.Other.score == 0 )
+        col.Other.TakeEvent(God.E(EventTypes.AddScore).Set(-1).Set(Who), true);
+
     }
     // RemoveInventory(ThingInfo) no idea what I'm going to do with this but I want to use this variable
     // Go to the attackaction and make it where you lose a point if hit with the hitend function, dont be afraid to copy code.
