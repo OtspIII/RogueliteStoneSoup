bf93dab6915dbc417ef72f4e1307a8c53eba065f
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-03-25 12:08:03 -0400

    Fixed grapple

--- Assets/Student Folders/SamsonW/Scripts/Traits/GrappleHookTrait.cs
+++ Assets/Student Folders/SamsonW/Scripts/Traits/GrappleHookTrait.cs
@@ -16,17 +16,34 @@ public class GrappleHookTrait : Trait
             case EventTypes.OnSpawn:
             {
                 ThingInfo grapplerThingInfo = e.GetThing(ThingEInfo.Source);
+                if (grapplerThingInfo == null)
+                {
+                    Debug.LogWarning("Grapple fail, shooter is null");
+                    return;
+                }
                 i.SetThing(ThingEInfo.Source, grapplerThingInfo); // Store the grappler source thing
                 break;
             }
             case EventTypes.OnDestroy:
             {
                 ThingInfo grapplerThingInfo = i.GetThing(ThingEInfo.Source);
+                if (grapplerThingInfo == null)
+                {
+                    Debug.LogWarning("Grapple fail, shooter is null");
+                    return;
+                }
 
+                float grappleDuration = i.GetFloat(NumInfo.Time);
+                if (grappleDuration <= 0)
+                {
+                    Debug.LogWarning("Grapple fail, duration is invalid");
+                    return;
+                }
+                
                 EventInfo changeActionEvent = God.E(EventTypes.StartAction);
                 changeActionEvent.SetAction(ActionInfo.Action, Actions.Grapple);
                 changeActionEvent.SetInt(NumInfo.Priority, 100); // High priority so thing is forced into action
-                changeActionEvent.SetFloat(NumInfo.Time, i.GetFloat(NumInfo.Time)); // Set grapple duration
+                changeActionEvent.SetFloat(NumInfo.Time, grappleDuration); // Set grapple duration
                 changeActionEvent.SetVector(VectorInfo.Position, e.GetVector()); // Set target hook position
                 grapplerThingInfo.TakeEvent(changeActionEvent);
                 break;
