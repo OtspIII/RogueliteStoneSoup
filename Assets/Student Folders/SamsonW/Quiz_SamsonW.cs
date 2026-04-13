c4917168cc1f6612e45c8e898f448ef2e72d45c4
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-04-13 12:39:36 -0400

    Finished quiz

--- Assets/Student Folders/SamsonW/Quiz_SamsonW.cs
+++ Assets/Student Folders/SamsonW/Quiz_SamsonW.cs
@@ -13,7 +13,11 @@ public class Quiz_SamsonW : QuizScript
     public override List<int> FilterEven(List<int> l)
     {
         List<int> r = new List<int>();
-        //Insert Filter Code Here
+        foreach (int n in l)
+        {
+            if(n % 2 == 0)
+                r.Add(n);
+        }
         return r;
     }
     
@@ -22,55 +26,84 @@ public class Quiz_SamsonW : QuizScript
         int r = -999;
         foreach (int n in l)
         {
-            //Insert Find Best Code Here
+            if (n > r)
+                r = n;
         }
         return r;
     }
 
     public override List<int> SortHighToLow(List<int> l)
     {
+        List<int> remaining = new List<int>(l); // copy of input
         List<int> r = new List<int>();
         int safety = 999;
-        while (safety > 0) //Needs an &&
+        while (safety > 0 && remaining.Count > 0) //Needs an &&
         {
             safety--;
-            //Insert Sort Code Here
+
+            int highest = int.MinValue;
+            foreach(int n in remaining)
+            {
+                if(n > highest)
+                    highest = n;
+            }
+            r.Add(highest);
+            remaining.Remove(highest);
         }
         return r;
     }
 
     public override int ReturnRandom(List<int> l)
     {
-        return l[0];
+        return l[Random.Range(0, l.Count)];
     }
 
     public override void RandomForEach(List<int> l)
     {
+        List<int> remaining = new List<int>(l); // make a copy
         int safety = 999;
-        while (safety > 0 && l.Count > 0)
+        while (safety > 0 && remaining.Count > 0)
         {
             safety--;
+
+            int randomValue = ReturnRandom(remaining);
+            Debug.Log(randomValue);
+            remaining.Remove(randomValue);
         }
     }
 
     public override List<int> Shuffle(List<int> l)
     {
+        List<int> remaining = new List<int>(l); // make a copy
         List<int> r = new List<int>();
         int safety = 999;
-        while (safety > 0 && l.Count > 0)
+        while (safety > 0 && remaining.Count > 0)
         {
             safety--;
+            
+            int randomValue = ReturnRandom(remaining);
+            r.Add(randomValue);
+            remaining.Remove(randomValue);
         }
         return r;
     }
 
     public override string WeightedRandom(Dictionary<string, float> d)
     {
-        //This one requires a lot more lines of code than the others
-        foreach (string k in d.Keys)
+        float totalWeight = 0;
+        foreach(float weight in d.Values)
+            totalWeight += weight;
+        
+        float randomTargetWeight = Random.Range(0, totalWeight);
+        float currWeight = 0;
+        foreach (var kvp in d)
         {
-            return k;
+            currWeight += kvp.Value;
+            
+            if(currWeight > randomTargetWeight)
+                return kvp.Key; // found the item
         }
+        
         return "";
     }
 }
