26f9b437e944579c61c590b7f3f71d6169a778c1
Michael <thelostkid224@gmail.com>
2026-04-22 12:03:26 -0400

    Level Setup

--- Assets/Student Folders/MichaelT/Level_MichaelT.cs
+++ Assets/Student Folders/MichaelT/Level_MichaelT.cs
@@ -5,12 +5,13 @@ public class Level_MichaelT : LevelBuilder
     public Level_MichaelT()
     {
         Author = Authors.MichaelT;
-        SpawnPlayer();
-        
-        Size= new Vector2Int(4,4); 
-        LinkOdds = 1;
     }
-
-    //public override void BuildGeoMap
+    
+    public override void Customize()
+    {
+        SpawnPlayer(); 
+        Size = new Vector2Int(4,4); 
+        LinkOdds = 1; 
+    }
     
 }
