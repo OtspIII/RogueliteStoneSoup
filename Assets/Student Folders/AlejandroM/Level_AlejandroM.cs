847270e1bc9266a125a0c2552ade8a2ee987d573
Roxas255 <aemartinez1224@icloud.com>
2026-05-06 16:33:44 -0400

    Level script update

--- Assets/Student Folders/AlejandroM/Level_AlejandroM.cs
+++ Assets/Student Folders/AlejandroM/Level_AlejandroM.cs
@@ -9,44 +9,40 @@ using UnityEngine;
 
     public override void Customize()
     {
-        SpawnPlayer(); // spawns player
+        SpawnPlayer();
 
-        Size = new Vector2Int(3, 3); //size is a 3x3
+        Size = new Vector2Int(1, 2);
 
-        LinkOdds = 0f; //a chance to create more rooms to connect
+        LinkOdds = 0f;
 
-        Boss = null; // no boss yet
+        Boss = null;
     }
 
     public override void BuildGeoMap()
-    {    // top row of T
-        AddGeo(new GeoTile(0, 2, this));
-        AddGeo(new GeoTile(1, 2, this));
-        AddGeo(new GeoTile(2, 2, this));
-        //middle row of T
-        AddGeo(new GeoTile(1, 1, this));
-        // bottom row of T
-        AddGeo(new GeoTile(1, 0, this));
+    {
+        AddGeo(new GeoTile(0, 0, this));
+        AddGeo(new GeoTile(0, 1, this));
     }
 
-    public override void BuildMainPath() //connects tiles
-    {   // sets player spwn 
-        PlayerSpawn = GetGeo(1, 0);
+    public override void FindQuotas()
+    {
+        Quotas.Clear();
+
+        
+        int enemyCount = God.Session.Level;
+
+        // Safety clamp so it does not get crazy
+        enemyCount = Mathf.Clamp(enemyCount, 1, 5);
+
+        Quotas.Add(new Tag("Hater", 1, enemyCount));
+
+        FindThings();
+    }
+
+    public override void BuildMainPath()
+    {
+        // Player spawns in the room 
+        PlayerSpawn = GetGeo(0, 0);
         PlayerSpawn.SetPath(GeoTile.GeoTileTypes.PlayerStart);
-        // sets exit
-        Exit = GetGeo(2, 2);
-        Exit.SetPath(GeoTile.GeoTileTypes.Exit);
-        // Connects the tiles 
-        GetGeo(1, 0).Links.Add(Directions.Up);
-        GetGeo(1, 1).Links.Add(Directions.Down);
-
-        GetGeo(1, 1).Links.Add(Directions.Up);
-        GetGeo(1, 2).Links.Add(Directions.Down);
-
-        GetGeo(1, 2).Links.Add(Directions.Left);
-        GetGeo(0, 2).Links.Add(Directions.Right);
-
-        GetGeo(1, 2).Links.Add(Directions.Right);
-        GetGeo(2, 2).Links.Add(Directions.Left);
     }
 }
